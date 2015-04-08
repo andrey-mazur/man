ac82517e37f9dc70f58f367f047ae87223697be2
Andrey Mazur <andrey_mazur@ukr.net>
2015-04-09 01:09:34 +0300

    Qt5 dependency added in cmake and sources

--- apps/host/main.cpp
+++ apps/host/main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 #include <boost/thread/thread.hpp>
 #include <asiosdk/common/asio.h>
+#include <QGuiApplication>
 
 int main(int argc, char *argv[])
 {
   std::cout << "TestApp" << std::endl;
+
+  QGuiApplication app(argc, argv);
+  app.exec();
+
   return 0;
 }
